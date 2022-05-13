184983ca6696c89b065497dd40ee7aed84a17df4
inovatormatin <manojgeetparmar@gmail.com>
2022-05-13 14:48:50 +0530

    Fixing bug

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
 const port = 3000;
 const app = express();
 
 app.get('/', (req, res) =>{
     res.send('Everything is good');
 });
-
-app.get('/pass', (req, res) =>{
-    res.send(
-        bcrypt.compare('manish', a, function(err, result) {
-            result == true;
-            console.log(result);
-        })
-    );
-});
-
 
 app.listen(process.env.PORT || port, () => {
     console.log(`server is running `)
