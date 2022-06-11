e8a2dc40989701cce1837c30cdef1b67f04c7dda
inovatormatin <manojgeetparmar@gmail.com>
2022-06-11 17:39:18 +0530

    Fix: issue

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 4000;
 const { connectdb } = require('./config/database');
+const cors = require('cors');
 require('dotenv').config();
 
 connectdb();
 app.use(cors());
 app.options("*", cors());
-app.use(express.json({
-    type: ['application/json', 'text/plain']
-}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // root
 app.get('/', (req, res) => {
@@ -19,7 +19,6 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/blogs', require('./routes/blogRoutes'));
 
-const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
