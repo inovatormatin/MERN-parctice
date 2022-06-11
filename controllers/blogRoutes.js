0a4cbc04d859860a62f0eeb9375b2cbf387197b0
inovatormatin <manojgeetparmar@gmail.com>
2022-06-11 06:52:29 +0530

    Fix: start script

--- controllers/blogRoutes.js
+++ controllers/blogRoutes.js
@@ -16,7 +16,7 @@ const getallblogs = async (req, res) => {
 // Route 2: Get All blogs by id
 const getblogbyid = async (req, res) => {
     try {
-        const blog = await Blog.find({authorid : req.user.id});
+        const blog = await Blog.find({ authorid: req.user.id });
         res.status(200).json(blog)
     } catch (error) {
         console.error(error.message);
@@ -32,12 +32,12 @@ const addblog = async (req, res) => {
         return res.status(400).json({ errors: errors.array() });
     }
     try {
-        const {title, description, img, author} = req.body;
+        const { title, description, img, author } = req.body;
         const blog = new Blog({
-            title, 
-            description, 
-            img, 
-            author, 
+            title,
+            description,
+            img,
+            author,
             authorid: req.user.id
         })
         const newBlog = await blog.save();
@@ -49,58 +49,51 @@ const addblog = async (req, res) => {
 };
 
 // Route 4: Update blog (login required)
-const updateblog = async(req, res) =>{
-    try{
+const updateblog = async (req, res) => {
+    try {
         // creating new object with updated data
         const newBlog = {};
-        const {title, description, img} = req.body;
-        if(title){newBlog.title = title};
-        if(description){newBlog.description = description};
-        if(img){newBlog.img = img};
-
+        const { title, description, img } = req.body;
+        if (title) { newBlog.title = title };
+        if (description) { newBlog.description = description };
+        if (img) { newBlog.img = img };
         // find blog by id which we want to update
         let optblog = await Blog.findById(req.params.id);
-        if(!optblog){
-            return res.status(404).json({error: "Blog not found"})
+        if (!optblog) {
+            return res.status(404).json({ error: "Blog not found" })
         }
-
         // checking for user access
-        if(optblog.authorid.toString() !== req.user.id){
+        if (optblog.authorid.toString() !== req.user.id) {
             return res.status(401).send("Not allowed")
         }
-
         // updating blog
-        optblog = await Blog.findByIdAndUpdate(req.params.id, {$set: newBlog}, {new: true});
+        optblog = await Blog.findByIdAndUpdate(req.params.id, { $set: newBlog }, { new: true });
         res.status(200).json(optblog);
 
-    } catch(error) {
+    } catch (error) {
         console.error(error.message);
-        res.status(500).json({error: "Internal server errror"});
+        res.status(500).json({ error: "Internal server errror" });
     }
 }
 
 // Route 5: Deleting exsiting blog (login required)
-const deleteblog = async(req, res) =>{
-    try{
-
+const deleteblog = async (req, res) => {
+    try {
         // find blog by id which we want to update
         let delblog = await Blog.findById(req.params.id);
-        if(!delblog){
-            return res.status(404).json({error: "Blog not found"})
+        if (!delblog) {
+            return res.status(404).json({ error: "Blog not found" })
         }
-
         // checking for user access
-        if(delblog.authorid.toString() !== req.user.id){
+        if (delblog.authorid.toString() !== req.user.id) {
             return res.status(401).send("Not allowed")
         }
-
         // updating blog
         delblog = await Blog.findByIdAndDelete(req.params.id);
-        res.status(200).json({success: "Blog deleted", delblog});
-
-    } catch(error) {
+        res.status(200).json({ success: "Blog deleted", delblog });
+    } catch (error) {
         console.error(error.message);
-        res.status(500).json({error: "Internal server errror"});
+        res.status(500).json({ error: "Internal server errror" });
     }
 }
 
